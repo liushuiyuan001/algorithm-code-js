ab3c6690208da1b3be046b4e4a51d4c0fcf5ea58
Liu Shuiyuan <13783534092@sina.cn>
2021-05-09 22:55:01 +0800

    漫画算法二叉堆

---
+++ src/MQ.js
@@ -0,0 +1,44 @@
+/**
+ * 上浮日调整
+ *
+ * @param {*} [arr=[]]
+ */
+function upAdjust(arr = []) {
+   const childIdx = arr.length - 1;
+   const parentIdx = parseInt((childIdx - 1) / 2);
+   //temp 报存插入的叶子节点值 用于最后的赋值
+   const temp = arr[childIdx]
+   while (childIdx > 0 && temp  < arr[parentIdx]) {
+         // 无须真正交换 单向赋值即可
+         arr[childIdx] = arr[parentIdx]
+         childIdx = parentIdx
+         parentIdx =  parseInt((parentIdx - 1) /  2)
+   }
+   arr[childIdx] = temp
+}
+
+/**
+ * 下沉调整
+ * @param {待调整的堆} arr 
+ * @param {要下沉的父节点} parentIdx 
+ * @param {堆的有效大小} len 
+ */
+function downAdjust(arr = [], parentIdx, len) {
+    // temp报存父节点值 用于最后的赋值
+    const temp = arr[parentIdx]
+    let childIdx = 2*parentIdx + 1
+    while(childIdx < len) {
+      //如果有右孩子 且右孩子小于左孩子的值 则定位到右孩子
+      if(childIdx + 1 < len && arr[childIdx + 1] < arr[childIdx]) {
+            childIdx++
+      }
+      // 如果父节点小于孩子节点的值则直接跳出
+      if(temp <= arr[childIdx]) {
+            break
+      }
+      arr[parentIdx] = arr[childIdx]
+      parentIdx = childIdx
+      childIdx = 2*parentIdx + 1
+    }
+    arr[parentIdx] = temp
+} 
--- src/tree.js
+++ src/tree.js
@@ -3,30 +3,31 @@ function TreeNode(val) {
       this.left = this.right = null
 }
 
-let  treeArr = [undefined,1,2,3,4,5,6,7]
+let  treeArr = [1,2,3,4,5,6,7]
 function getTreeNode(i) {
       let node = new TreeNode()
       node.val = treeArr[i]
 
-      if (2*i <= treeArr.length - 1) {
-            node.left = getTreeNode(2*i) 
-      }
       if (2*i + 1 <= treeArr.length - 1) {
-            node.right = getTreeNode(2*i + 1) 
+            node.left = getTreeNode(2*i + 1) 
+      }
+      if (2*i + 2 <= treeArr.length - 1) { 
+            node.right = getTreeNode(2*i + 2) 
       }
       return node
 }
-let node = getTreeNode(1)
+let node = getTreeNode(0)
 window.node = node
 
+// 前序遍历
+function preOrderTraversal(node) {
+    if(!node) return
+    console.log(node.val)
+    preOrderTraversal(node.left)
+    preOrderTraversal(node.right)
+}
 
-// function preOrderTraversal(node) {
-//     if(!node) return
-//     console.log(node.val)
-//     preOrderTraversal(node.left)
-//     preOrderTraversal(node.right)
-// }
-
+// 中序遍历         
 // function midOrderTraversal(node) {
 //     if(!node) return
 //     midOrderTraversal(node.left);
@@ -34,10 +35,44 @@ window.node = node
 //     midOrderTraversal(node.right);
 // }
 
-function postOrderTraversal(node){
-      if(!node) return;
-      postOrderTraversal(node.left);
-      postOrderTraversal(node.right);
-      console.log(node.val);
+// 后序遍历
+// function postOrderTraversal(node){
+//       if(!node) return;
+//       postOrderTraversal(node.left);
+//       postOrderTraversal(node.right);
+//       console.log(node.val);
+// }
+// postOrderTraversal(node)
+
+// 二叉树非递归前序遍历
+function stackTraversal(node) {
+   let stack  = []
+   let treeNode = node
+   while(stack.length !== 0 || treeNode) {
+         while(treeNode) {
+            console.log(treeNode.val)
+            stack.push(treeNode)
+            treeNode = treeNode.left
+        }
+        // 如果节点没有左孩子 则弹出栈节点 访问节点右孩子
+        if(stack.length) {
+            treeNode = stack.pop()
+            treeNode = treeNode.right
+        }
+   }
 }
-postOrderTraversal(node)
+// preOrderTraversal(node)
+// console.log('================================')
+// stackTraversal(node)
+
+//  二叉树层序遍历
+function levelOrderTraversal(node) {
+      let queue = [node]
+      while(queue.length) {
+		const treeNode = queue.shift()
+		console.log(treeNode.val)
+		treeNode.left && queue.push(treeNode.left)
+		treeNode.right && queue.push(treeNode.right)
+      }
+}
+levelOrderTraversal(node)
